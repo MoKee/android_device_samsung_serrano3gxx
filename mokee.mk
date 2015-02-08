c0c38d073f6d98f0ea5438322b3be5942d3c3413
martincz <martincz@mokeedev.com>
2015-02-08 13:46:30 +0800

    serrano3gxx: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
 
@@ -10,4 +10,4 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gxx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
 
 PRODUCT_DEVICE := serrano3gxx
-PRODUCT_NAME := cm_serrano3gxx
+PRODUCT_NAME := mk_serrano3gxx
